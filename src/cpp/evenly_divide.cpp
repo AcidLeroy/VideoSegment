b4f5cff1095fec0c8af6343258a790b0639f6a74
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-03-29 13:15:10 -0600

    Add more guards

--- src/cpp/evenly_divide.cpp
+++ src/cpp/evenly_divide.cpp
@@ -1,11 +1,27 @@
 // Copyright (c) 2015 <Cody W. Eilar>
-#include "evenly_divide.h" // NOLINT
+#include "evenly_divide.h"  // NOLINT
+#include <iostream>
+#include <exception>
 #include <vector>
+#include "opencv2/opencv.hpp"
+
 EvenlyDivide::EvenlyDivide() {}
 EvenlyDivide::~EvenlyDivide() {}
 
-void EvenlyDivide::SegmentVideo(const cv::VideoCapture &vc,
+void EvenlyDivide::SegmentVideo(cv::VideoCapture &vc,
                                 std::vector<int> *indicies) {
+  if (!vc.isOpened()) {
+    throw std::runtime_error("VideoCapture is not open!");
+    return;
+  }
+
+  int count = 0;
+  cv::Mat frame;
+  while (1) {
+    vc >> frame;
+    if (frame.empty()) break;
+    count++;
+  }
   indicies->push_back(1);
   indicies->push_back(2);
   indicies->push_back(3);
