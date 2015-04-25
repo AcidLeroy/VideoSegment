2ed2e0feccff2555566a93953203b36cd905d18f
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-04-24 21:17:47 -0600

    Add ability to print output to file
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- src/cpp/programs/select_frames.cpp
+++ src/cpp/programs/select_frames.cpp
@@ -4,14 +4,24 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <vector>
-#include <iostream> 
+#include <iostream>
 #include <string>
 
 int main(int argc, char *argv[]) {
   std::string filename = "/Users/cody/test.mov";
   cv::VideoCapture vc(filename);
-  UserDivide ud; 
-  std::vector<Segment> segments;  
-  ud.SegmentVideo(&vc, &segments);  
+  UserDivide ud;
+  std::vector<Segment> segments;
+  ud.SegmentVideo(&vc, &segments);
+  std::ofstream out("/tmp/test.txt");
+  std::streambuf *coutbuf = std::cout.rdbuf();
+  std::cout.rdbuf(out.rdbuf());
+  std::cout << "begin, end\n";
+  for (int i = 0; i < segments.size(); ++i) {
+    std::cout << segments[i].begin << ", " << segments[i].end << "\n";
+  }
+  std::cout.rdbuf(coutbuf);
+  out.close();
+
   return 0;
 }
