f08a8fd28afd7e897cc1c6a298e1c1aa0127470d
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-04-27 22:16:25 -0600

    Refactor method accuracy
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- lib/method_accuracy/method_accuracy_test.cpp
+++ lib/method_accuracy/method_accuracy_test.cpp
@@ -1,54 +1,6 @@
-#include "segmenter_interface.h"
+
+#include "method_accuracy.h"
 #include <gtest/gtest.h>
-#include <vector>
-#include <iterator>
-
-class MethodAccuracy {
- public:
-  template <typename T>
-  static size_t NumFalsePositives(const std::vector<T> &gnd_truth,
-                                  const std::vector<T> &predicted,
-                                  size_t length) {
-    size_t count = 0;
-    for (auto &pred : predicted) {
-      auto it = find(gnd_truth.begin(), gnd_truth.end(), pred);
-      if (it == gnd_truth.end()) {
-        count++;
-      }
-    }
-    return count;
-  }
-
-  template <typename T>
-  static size_t NumTrueNegatives(const std::vector<T> &gnd_truth,
-                                 const std::vector<T> &predicted,
-                                 size_t length) {
-    size_t fp = NumFalsePositives(gnd_truth, predicted, length);
-    size_t fn = NumFalseNegatives(gnd_truth, predicted, length);
-    size_t tp = NumTruePositives(gnd_truth, predicted, length);
-    return length - (fp + fn + tp);
-  }
-
-  template <typename T>
-  static size_t NumFalseNegatives(const std::vector<T> &gnd_truth,
-                                  const std::vector<T> &predicted,
-                                  size_t length) {
-    return NumFalsePositives(predicted, gnd_truth, length);
-  }
-  template <typename T>
-  static size_t NumTruePositives(const std::vector<T> &gnd_truth,
-                                 const std::vector<T> &predicted,
-                                 size_t length) {
-    size_t count = 0;
-    for (auto &pred : predicted) {
-      auto it = find(gnd_truth.begin(), gnd_truth.end(), pred);
-      if (it != gnd_truth.end()) {
-        count++;
-      }
-    }
-    return count;
-  }
-};
 
 class TestMethodAccuracy : public ::testing::Test {
  protected:
