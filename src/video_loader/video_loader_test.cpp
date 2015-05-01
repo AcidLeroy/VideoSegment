8fdf69d1f32946249c14f782646bcab55647c137
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-05-01 09:50:41 -0600

    Add basics of video loader
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- src/video_loader/video_loader_test.cpp
+++ src/video_loader/video_loader_test.cpp
@@ -1,5 +1,15 @@
 #include <gtest/gtest.h>
+#include <vector>
+
+class VideoLoader {};
 
 class VideoLoaderTest : public ::testing::Test {
  protected:
+  VideoLoader vl;
 };
+
+TEST_F(VideoLoaderTest, TestCorrectSize) {
+  using namespace std;
+  double buff_size_mb = 8;
+  // vector<vector<double>> mat = vl.LoadVideo();
+}
