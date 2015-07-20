388bc4a98413e3b3cbfb3fb9d37797da874b9a19
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-07-20 07:03:41 -0600

    Fix bounds problems
    - Add function to read a range of frames
    - Get dimensions from frame (still a better way to do it probably)
    
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- python/read_video.py
+++ python/read_video.py
@@ -9,14 +9,15 @@ class VideoHelper:
                 format(filename))
         cv_total_frames = cv2.CAP_PROP_FRAME_COUNT
         self.total_frames = int(self.cap.get(cv_total_frames))
+        self.frame_shape = self.ReadFrame(0).shape;
 
     def ConvertToGray(self, frame): 
         return(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)); 
 
     def ReadFrame(self, frame_number): 
         # Verify that we are in bounds
-        if (frame_number > self.total_frames): 
-           frame_number = self.total_frames 
+        if (frame_number > self.total_frames-1): 
+           frame_number = self.total_frames-2 
         self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
         ret, frame = self.cap.read()
         if not ret:
@@ -26,6 +27,22 @@ class VideoHelper:
         b,g,r = cv2.split(frame)       # get b,g,r
         frame = cv2.merge([r,g,b])     # switch it to rgb
         return frame
+
+    def ReadFrameRange(self, start, stop):
+        if stop < start: 
+            start, stop = stop, start
+        if start < 0: 
+            start = 0; 
+        if stop > self.total_frames - 1: 
+            stop = self.total_frames - 1; 
+
+        frame_buffer = np.zeros((stop-start+1,) + self.frame_shape, dtype=np.uint8) 
+        
+        for idx, val in enumerate(range(start, stop+1)): 
+            frame_buffer[idx, ...] = self.ReadFrame(val)
+
+        return frame_buffer
+
 
     def ReadToBuffer(self, buf_size_mb): 
         buf_size_b = 1e6 * buf_size_mb
@@ -38,7 +55,7 @@ class VideoHelper:
             num_frames = self.total_frames
 
         # Preallocate frames
-        frame_buffer = np.zeros((num_frames,) + frame.shape, dtype=np.uint8) 
+        frame_buffer = np.zeros((num_frames,) + self.frame_shape, dtype=np.uint8) 
         frame_buffer[0, ...] = frame
         for i in range(1, num_frames): 
             frame_buffer[i, ...] = self.ReadFrame(i)
